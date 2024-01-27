143ecd9239ed4f304ad2efe1320f262582f49c97
nvarma <nvarma@ivycomptech.com>
2024-01-27 16:23:21 +0530

    playing music on onload of page

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 let scene;
 let clone, sunflower, rose;
 window.onload = function () {
-    scene = document.querySelector("a-scene");
-    sunflower = document.getElementById("sunflowerModel");
-    for (let i = 0; i < 1; i++) {
-        clone = sunflower.cloneNode(true);
-        clone.setAttribute("position", { x: -5.90*i, y: 0, z: 0 });
-        scene.append(clone);
-    }
+    // scene = document.querySelector("a-scene");
+    // sunflower = document.getElementById("sunflowerModel");
+    // for (let i = 0; i < 1; i++) {
+    //     clone = sunflower.cloneNode(true);
+    //     clone.setAttribute("position", { x: -5.90*i, y: 0, z: 0 });
+    //     scene.append(clone);
+    // }
+
+    var entity = document.querySelector('[sound]');
+    entity.components.sound.playSound();
+
 }
